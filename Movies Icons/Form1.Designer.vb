273dfea35c368c201075884753635750f48f5543
DrAliRagab <dr.aliragab@gmail.com>
2018-06-06 02:55:22 +0200

    Fix Some Bugs

--- Movies Icons/Form1.Designer.vb
+++ Movies Icons/Form1.Designer.vb
@@ -93,7 +93,6 @@ Partial Class Form1
         Me.TextBox1.ReadOnly = True
         Me.TextBox1.Size = New System.Drawing.Size(591, 20)
         Me.TextBox1.TabIndex = 0
-        Me.TextBox1.Text = "D:\11\Movies"
         '
         'Button1
         '
